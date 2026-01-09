13d15155af512032d7569b1cec33a955a896f2a1
Om Patel <202273921+ompatel100@users.noreply.github.com>
2026-01-10 01:22:20 +0530

    feat: add detailed console logging for feedback on terminal and skip cooldown for last search

--- src/app.js
+++ src/app.js
@@ -5,21 +5,44 @@ import { generateQueries } from "./queryGenerator.js";
 import { config, delay, randomInt, type, clear } from "./utils.js";
 
 program
+  .name("microsoft-rewards-automator")
+  .description(
+    "A Microsoft Rewards automation tool for Edge featuring stealth, dynamic query generation, multiple profile support and extensive configuration."
+  )
   .option("-p, --profile <key>", "profile key in config")
   .option("-c, --count <number>", "search count", parseInt)
   .option("--min <number>", "minimum random searches", parseInt)
   .option("--max <number>", "maximum random searches", parseInt)
   .option("-H, --headless", "headless mode")
+  .option("--no-headless", "headed mode")
   .parse(process.argv);
 
 const opts = program.opts();
+
+console.log("----------------------------------------------------------");
+console.log(`    __  _________________  ____  _____ ____  ____________
+   /  |/  /  _/ ____/ __ \\/ __ \\/ ___// __ \\/ ____/_  __/
+  / /|_/ // // /   / /_/ / / / /\\__ \\/ / / / /_    / /
+ / /  / // // /___/ _, _/ /_/ /___/ / /_/ / __/   / /
+/_/  /_/___/\\____/_/ |_|\\____//____/\\____/_/     /_/`);
+console.log(`    ____  _______       _____    ____  ____  _____
+   / __ \\/ ____/ |     / /   |  / __ \\/ __ \\/ ___/
+  / /_/ / __/  | | /| / / /| | / /_/ / / / /\\__ \\
+ / _, _/ /___  | |/ |/ / ___ |/ _, _/ /_/ /___/ /
+/_/ |_/_____/  |__/|__/_/  |_/_/ |_/_____//____/`);
+console.log(`    ___   __  ____________  __  ______  __________  ____
+   /   | / / / /_  __/ __ \\/  |/  /   |/_  __/ __ \\/ __ \\
+  / /| |/ / / / / / / / / / /|_/ / /| | / / / / / / /_/ /
+ / ___ / /_/ / / / / /_/ / /  / / ___ |/ / / /_/ / _, _/
+/_/  |_\\____/ /_/  \\____/_/  /_/_/  |_/_/  \\____/_/ |_|\n`);
+console.log("----------------------------------------------------------\n");
 
 const { profileList, defaultProfileKey } = config.profiles;
 let profileKey = opts.profile;
 
 if (!profileKey) {
   console.log(
-    `No profile specified (--profile). Using default config profile: ${defaultProfileKey}`
+    "No profile specified (--profile). Using default config profile.\n"
   );
   profileKey = defaultProfileKey;
 }
@@ -48,9 +71,13 @@ puppeteer.use(StealthPlugin());
 const isHeadless =
   opts.headless !== undefined ? opts.headless : config.browser.headless;
 
-if (isHeadless) {
-  console.log("Running in headless mode");
-}
+console.log(`- Profile: ${profileDir} (${profileKey})`);
+console.log(`- Searches: ${searchCount}`);
+console.log(`- Mode: ${isHeadless ? "Headless" : "Headed"}\n`);
+
+console.log(`Generated ${queries.length} unique queries.`);
+
+console.log("Opening browser...");
 
 let browser;
 
@@ -64,6 +91,7 @@ try {
 
   const page = (await browser.pages())[0];
 
+  console.log("Visiting bing...\n");
   await page.goto("https://www.bing.com");
 
   for (let i = 0; i < queries.length; i++) {
@@ -74,18 +102,27 @@ try {
       await clear(page, searchBoxSelector);
     }
 
+    console.log(`[${i + 1}/${queries.length}] Searching: ${queries[i]}`);
+
     await type(page, searchBoxSelector, queries[i]);
 
     await page.keyboard.press("Enter");
 
-    const cooldown = randomInt(
-      config.delays.cooldown.min,
-      config.delays.cooldown.max
-    );
-    await delay(cooldown);
+    if (i < queries.length - 1) {
+      const cooldown = randomInt(
+        config.delays.cooldown.min,
+        config.delays.cooldown.max
+      );
+
+      console.log(`Cooling down ${(cooldown / 1000).toFixed(1)} sec\n`);
+      await delay(cooldown);
+    }
   }
+
+  console.log(`All ${queries.length} searches completed successfully.\n`);
 } catch (err) {
-  console.error("Error: " + err);
+  console.log();
+  console.error(err);
 } finally {
   if (browser) {
     await browser.close();
